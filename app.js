28f1021ac9a18ef85fa316922ac0e3a687a400ba
mn39 <48751102+mn39@users.noreply.github.com>
2021-05-21 13:43:38 +0900

    robots update

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var path = require('path');
 
 
 
-var express = require('express')
-, http = require('http')
-, app = express()
-, server = http.createServer(app);
+var express = require('express'),
+    http = require('http'),
+    app = express(),
+    server = http.createServer(app);
 
 
 // 화면 engine을 ejs로 설정
@@ -15,20 +15,32 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 app.get('/', function (req, res) {
-    res.render(path.join(__dirname+'/index.html'));
+    res.render(path.join(__dirname + '/index.html'));
+});
+
+
+app.get('/robots.txt', function (req, res) {
+    res.type("text/plain");
+    res.send(
+        "User-agent: *\nAllow: /\n"
+    );
+});
+
+app.get('/sitemap.xml', function (req, res) {
+    res.sendFile(path.join(__dirname + '/sitemap.xml'));
 });
 
 app.get('/search', function (req, res) {
-    getPlayTime(res,req.query.name)
-    
+    getPlayTime(res, req.query.name)
+
 });
 
-server.listen(process.env.PORT||8000, function() {
+server.listen(process.env.PORT || 8000, function () {
     console.log('Express server listening on port ' + server.address().port);
 });
 
 
-function getPlayTime(res,name){
+function getPlayTime(res, name) {
     var urlname = urlencode(name)
     var URL = 'https://fow.kr/find/' + urlname
 
@@ -36,17 +48,18 @@ function getPlayTime(res,name){
 
     request(URL, function (error, response, html) { // get played time
         if (error) {
-            console.log(error,'error in request')
+            console.log(error, 'error in request')
         };
-        
+
         htmlSplit = html.split("'tipsy_live' tipsy='")
         var result = NaN
-        if(htmlSplit.length>2){
-            result =  htmlSplit[2].slice(0,10)+":"+htmlSplit[2].slice(11,19)
+        if (htmlSplit.length > 2) {
+            result = htmlSplit[2].slice(0, 10) + ":" + htmlSplit[2].slice(11, 19)
         }
-        res.render(path.join(__dirname+'/search.html'),{sum_name:name,result:result});
+        res.render(path.join(__dirname + '/search.html'), {
+            sum_name: name,
+            result: result
+        });
     });
 
 }
-
-
